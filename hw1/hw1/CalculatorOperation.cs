9051e4024a2607b4bdfa6ff7dba480a011d519aa
TimSon777 <dusalimov.tima@mail.ru>
2021-09-10 20:15:34 +0300

    Add hw1

---
+++ hw1/hw1/CalculatorOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace hw1
+{
+    public enum CalculatorOperation : byte
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide
+    }
+}
---
+++ hw1/hw1/Parser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hw1
+{
+    public static class Parser
+    {
+        private const int WrongArgLength = 1;
+        private const int WrongArgFormatInt = 2;
+        private const int WrongArgFormatOperation = 3;
+
+        public static int ParseCalcArguments(string[] args, out int val1, out CalculatorOperation operation, out int val2)
+        {
+            val1 = val2 = (int) (operation = 0);
+            if (!CheckArgLength(args.Length)) return WrongArgLength;
+            if (!TryParseIntOrQuit(args[0], out val1) || !TryParseIntOrQuit(args[2], out val2)) return WrongArgFormatInt;
+            return !TryParseOperationOrQuit(args[1], out operation) ? WrongArgFormatOperation : 0;
+        }
+        
+        private static bool CheckArgLength(int length)
+        {
+            if (length == 3) return true;
+            Console.WriteLine($"The program requires 3 arguments, but was {length}");
+            return false;
+        }
+        
+        private static bool TryParseOperationOrQuit(string arg, out CalculatorOperation operation)
+        {
+            operation = arg switch
+            {
+                "-" => CalculatorOperation.Minus,
+                "*" => CalculatorOperation.Multiply,
+                "/" => CalculatorOperation.Divide,
+                _ => CalculatorOperation.Plus
+            };
+
+            return operation != CalculatorOperation.Plus || arg == "+";
+        }
+
+        private static bool TryParseIntOrQuit(string arg, out int val)
+        {
+            if (int.TryParse(arg, out val)) return true;
+            Console.WriteLine($"Value is not int: {arg}");
+            return false;
+        }
+    }
+}
