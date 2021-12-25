5e687b3a5cba718e2f8c907213751335e5b61ecb
flayexz <hxjsnsjdkswb@mail.ru>
2021-12-25 21:39:54 +0300

    added gameSession

---
+++ GameSegments/TCPServer/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TCPServer
+{
+    public class GameSession
+    {
+        public readonly Guid Guid;
+        public readonly TcpClient HostUser;
+        public TcpClient ConnectedUser { get; set; }
+        public GameSession(Guid guid, TcpClient hostUser, TcpClient connectedUser)
+        {
+            Guid = guid;
+            HostUser = hostUser;
+            ConnectedUser = connectedUser;
+        }
+    }
+}
