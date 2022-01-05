65a1e1bada9de4f1687a7009b7eae5b71776ab76
flayexz <hxjsnsjdkswb@mail.ru>
2022-01-05 21:24:53 +0300

    added xclient

---
+++ GameSegments/XProtocol/XPacketCreateLobby.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using XProtocol.Serializator;
+using XClient;
+
+namespace XProtocol
+{
+    public class XPacketCreateLobby
+    { 
+        [XField(2)]public Client HostUser;
+    }
+}
---
+++ GameSegments/XProtocol/XPacketLobbyCreated.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using XProtocol.Serializator;
+
+namespace XProtocol
+{
+    public class XPacketLobbyCreated
+    {
+        [XField(3)]public Guid LobbyGuid;
+    }
+}
